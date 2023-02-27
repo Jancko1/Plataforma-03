a1f29eaf5f0f06f8de7af9411960860363e9540f
xKeros <johanquiros968@gmail.com>
2023-02-26 20:36:45 -0600

    Add:Conection-DB

---
+++ server/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const conenctDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      "mongodb+srv://Pruebas:UwZAdPx0HxnHWKpr@plataforma03-v2.ka75tw6.mongodb.net/test"
+    );
+    console.log(`Conecion Exitosa ${conn.Connection.name}`);
+  } catch {
+    console.error(`Conexion erronia ${error.message}`);
+    process.exit(1)
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import { startApolloServe } from "./App.js";
 import {typeDefs} from './graphql/typeDefs.js'
 import { resolvers } from "./graphql/resolvers.js";
+import {conenctDB} from './db.js'
 
+conenctDB()
 
 startApolloServe(typeDefs, resolvers)
