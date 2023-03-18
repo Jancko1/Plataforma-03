a6278cb10c0440bab4eefbf4056279af62d9a0bc
xKeros <johanquiros968@gmail.com>
2023-03-18 10:40:28 -0600

    Add: Nueva base

--- server/DB/config.js
+++ server/DB/config.js
@@ -1,1 +1,1 @@
-export const MONGO_URI = 'mongodb+srv://Pruebas:UwZAdPx0HxnHWKpr@plataforma03-v2.ka75tw6.mongodb.net/test'
+export const MONGO_URI = 'mongodb+srv://plataformacircuito03:dUqqZfrMT17NG1z0@db-circuito-03.obf7mu1.mongodb.net/post-collection';
