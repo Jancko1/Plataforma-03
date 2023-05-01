ead8cf13703a84e1c3c78083d3b212eaaed80c43
LDani24 <ldsanchezu@gmail.com>
2023-04-30 23:46:20 -0600

    login y register

--- client/src/Components/Auth/Login.jsx
+++ client/src/Components/Auth/Login.jsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import React from "react";
+//Imagenes
+import wave from "../../img/wave.png";
+import bg from "../../img/loginUser.png";
+import avatar from "../../img/user.png";
+//Remix icon
+import "remixicon/fonts/remixicon.css";
+//ESTILOS CSS
+import "../../styles/Login.css";
+
 
 const Login = () => {
-  return (
-    <div>
-      Hola
-    </div>
-  )
-}
+  //Animacion al clickear un input
+  const inputs = document.querySelectorAll(".input");
+  function addcl() {
+    let parent = this.parentNode.parentNode;
+    parent.classList.add("focus");
+  }
+  function remcl() {
+    let parent = this.parentNode.parentNode;
+    if (this.value == "") {
+      parent.classList.remove("focus");
+    }
+  }
 
-export default Login
+  inputs.forEach((input) => {
+    input.addEventListener("focus", addcl);
+    input.addEventListener("blur", remcl);
+  });
+
+  return (
+    <div className="container">
+      <img src={wave} className="wave" alt="" />
+      <div className="img">
+        <img src={bg} alt="" />
+      </div>
+      <div className="login-content">
+        <form action="">
+          <img src={avatar} alt="" />
+          <h2 className="title">Bienvenido</h2>
+          <div className="input-div one">
+            {/* ------------USUARIO------------ */}
+            <div className="i">
+              <i class="ri-user-fill"></i>
+            </div>
+            <div className="div">
+              <h5>Usuario</h5>
+              <input type="text" className="input" />
+            </div>
+          </div>
+          {/* ------------CONTRASEÑA------------ */}
+          <div className="input-div pass">
+            <div className="i">
+              <i className="ri-lock-fill"></i>
+            </div>
+            <div className="div">
+              <h5>Contraseña</h5>
+              <input type="password" className="input" />
+            </div>
+          </div>
+          <input type="submit" className="boton" value="Iniciar sesión" />
+          <a href="#">Registrarse aquí</a>
+          {/* AQUÍ DEBE REDIRECCIONAR AL REGISTRO */}
+          <a href="#">¿Olvidaste la contraseña?</a>
+          
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- client/src/Components/Auth/SignUp.jsx
+++ client/src/Components/Auth/SignUp.jsx
@@ -1,11 +1,91 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+//Imagenes
+import wave from "../../img/wave.png";
+import bg from "../../img/bg.png";
+import avatar from "../../img/user.png";
+//Remix icon
+import "remixicon/fonts/remixicon.css";
+//ESTILOS CSS
+import "../../styles/Login.css";
 
 const SignUp = () => {
-  return (
-    <div>
-      Registro
-    </div>
-  )
-}
 
-export default SignUp
+  //Animacion al clickear un input
+  const inputs = document.querySelectorAll(".input");
+  function addcl() {
+    let parent = this.parentNode.parentNode;
+    parent.classList.add("focus");
+  }
+  function remcl() {
+    let parent = this.parentNode.parentNode;
+    if (this.value == "") {
+      parent.classList.remove("focus");
+    }
+  }
+
+  inputs.forEach((input) => {
+    input.addEventListener("focus", addcl);
+    input.addEventListener("blur", remcl);
+  });
+
+  return (
+    <div className="container">
+      <img src={wave} className="wave" alt="" />
+      <div className="img">
+        <img src={bg} alt="" />
+      </div>
+      <div className="login-content">
+        <form action="">
+          <img src={avatar} alt="" />
+          <h2 className="title">Registro</h2>
+          {/* ------------CORREO ELECTRONICO------------ */}
+          <div className="input-div one">
+            <div className="i">
+              <i class="ri-mail-line"></i>
+            </div>
+            <div className="div">
+              <h5>Correo electrónico</h5>
+              <input type="text" className="input" />
+            </div>
+          </div>
+          {/* ------------USUARIO------------ */}
+          <div className="input-div one">
+            <div className="i">
+              <i class="ri-user-fill"></i>
+            </div>
+            <div className="div">
+              <h5>Usuario</h5>
+              <input type="text" className="input" />
+            </div>
+          </div>
+          {/* ------------CONTRASEÑA------------ */}
+          <div className="input-div pass">
+            <div className="i">
+              <i className="ri-lock-fill"></i>
+            </div>
+            <div className="div">
+              <h5>Contraseña</h5>
+              <input type="password" className="input" />
+            </div>
+          </div>
+          {/* ------------REPETIR CONTRASEÑA------------ */}
+          <div className="input-div pass">
+            <div className="i">
+              <i className="ri-lock-fill"></i>
+            </div>
+            <div className="div">
+              <h5>Contraseña</h5>
+              <input type="password" className="input" />
+            </div>
+          </div>
+          <input type="submit" className="boton" value="Iniciar sesión" />
+          <a href="#">Registrarse aquí</a>
+          <a href="#">¿Olvidaste la contraseña?</a>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
